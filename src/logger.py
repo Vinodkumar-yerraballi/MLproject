c0a429463ce833956c2acdb465d872aefab81f04
unknown <vinodkumar9676150@gmail.com>
2023-03-09 12:59:37 +0530

    update files

--- src/logger.py
+++ src/logger.py
@@ -9,11 +9,11 @@ LOG_FILE_PATH=os.path.join(log_path,LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(asctime)s] %(lineno)d %(name)s- %(levelname)s - %(message)s"
-    level=logging.info(),
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+
+
 )
 
 
 
-if __name__=="__main__":
-    logging.info("logging has started")
